433b4a06db422e85ed40432daf44e2d609a1e23b
andreibtm <vanvuandrei676@gmail.com>
2025-11-15 10:59:21 +0200

    redo

--- app/src/screens/signup.screen.jsx
+++
@@ -1,9 +0,0 @@
-import {View, Button, Text} from "react-native"
-
-export function SignUpScreen({navigation}) {
-    return (
-        <View>
-            <Text>SignUp</Text>
-        </View>
-    )
-}
